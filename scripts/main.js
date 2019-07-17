967b8cadc383ca60d738f17bc1a81e01f4514a97
ma321b <aa.muhammad1030@gmail.com>
2019-07-17 16:52:15 +0500

    Website in its  initial stages

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';               // Changed heading to "Hello World!"
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function()
+{
+  mySrc = myImage.getAttribute('src')
+  if(mySrc === 'images/index.jpeg')
+  {
+    myImage.setAttribute('src','images/index2.jpeg')
+  }
+  else
+  {
+    myImage.setAttribute('src','images/index.jpeg')
+  }
+}
