5ff3255bd1b74ee028fffafe88dd84a0639ce623
ma321b <aa.muhammad1030@gmail.com>
2019-07-18 00:18:52 +0500

    Name prompt added + minor tweaks to main.js and index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';               // Changed heading to "Hello World!"
 
+/*
 document.querySelector('html').onclick = function() 
 {
-    alert('Ouch! Stop poking me!');
+    alert('Ouch! Stop poking me!');   //Pop up whenever user clicks on webapp
 }
+*/
 
 let myImage = document.querySelector('img');
 
@@ -19,4 +21,29 @@ myImage.onclick = function()
   {
     myImage.setAttribute('src','images/index.jpeg')
   }
+}
+
+
+let myButton = document.querySelector('button');
+
+function setUserName()
+{
+  let myName = prompt('Please enter your name (press OK to continue) ');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Ruby on Rails is awesome, ' + myName;
+}
+
+if(!localStorage.getItem('name'))
+{
+  setUserName();
+}
+else
+{
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Ruby on Rails is awesome, ' + storedName;
+}
+
+myButton.onclick = function()
+{
+  setUserName();
 }
